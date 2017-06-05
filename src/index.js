fe534ef80a18dee8c8c419753c39f1a47d9469bd
Craig Paul <craig.paul@coconutcalendar.com>
2017-06-05 08:53:49 -0600

    Adds ability to use full url and query string parameters

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 /* eslint-disable no-undef */
+import Utils from './utils';
+
 class CoconutCalendar {
-  constructor(subdomain, width = '100%', height = '580px') {
+  constructor(path, params = {}, width = '100%', height = '580px') {
+    this.path = path;
     this.width = width;
     this.height = height;
-    this.subdomain = subdomain;
+    this.params = Utils.parameterize(params);
     this.iframe = document.createElement('iframe');
 
     this.iframe.setAttribute('seamless', true);
     this.iframe.setAttribute('frameborder', 0);
     this.iframe.setAttribute('width', this.width);
     this.iframe.setAttribute('height', this.height);
-    this.iframe.setAttribute('src', `https://${this.subdomain}.coconutcalendar.com/embed`);
+
+    const url = Utils.isFullUrl(this.path) ? `${this.path}/embed` : `https://${this.path}.coconutcalendar.com/embed`;
+    this.iframe.setAttribute('src', `${url}${this.params}`);
   }
 
   embed(id) {
--- test/embed.spec.js
+++ test/embed.spec.js
@@ -1,18 +1,45 @@
+/* eslint-disable no-undef */
 import test from 'ava';
 import BrowserEnv from 'browser-env';
 import CoconutCalendar from '../src/index';
 
 BrowserEnv(['document']);
 
-test('Inserts proper iframe into DOM', t => {
+test('Inserts proper iframe into DOM', (t) => {
   const div = document.createElement('div');
-  div.id = 'test';
+  div.id = 'test1';
   document.body.appendChild(div);
 
-  const coconut = new CoconutCalendar('bell');
-  coconut.embed('test');
+  const coconut = new CoconutCalendar('example');
+  coconut.embed('test1');
 
-  const iframe = document.querySelector('iframe');
+  const iframe = document.querySelector('#test1 iframe');
   t.truthy(iframe);
-  t.true(iframe.getAttribute('src').includes('bell'));
+  t.true(iframe.getAttribute('src').includes('example'));
+});
+
+test('Uses full url if one is provided', (t) => {
+  const div = document.createElement('div');
+  div.id = 'test2';
+  document.body.appendChild(div);
+
+  const coconut = new CoconutCalendar('https://example.com');
+  coconut.embed('test2');
+
+  const iframe = document.querySelector('#test2 iframe');
+  t.true(iframe.getAttribute('src').includes('https://example.com'));
+});
+
+test('Includes query string if object provided', (t) => {
+  const div = document.createElement('div');
+  div.id = 'test3';
+  document.body.appendChild(div);
+
+  const coconut = new CoconutCalendar('example', {
+    key: 'value',
+  });
+  coconut.embed('test3');
+
+  const iframe = document.querySelector('#test3 iframe');
+  t.true(iframe.getAttribute('src').includes('?key=value'));
 });
